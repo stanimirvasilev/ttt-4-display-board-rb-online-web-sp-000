9a30150ad091196ac551f0442c060118d939fdae
Stanimir Vasilev <stanimirvasilev@yahoo.com>
2019-07-06 10:44:15 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -3,7 +3,15 @@
 board = [" "," "," "," "," "," "," "," "," "]
 def display_board(board)
   puts "#{board [0]}","#{board [1]}","#{board [2]}","#{board [3]}","#{board [4]}","#{board [5]}","#{board [6]}","#{board [7]}","#{board [8]}"
-  
+  puts "   |   |   "
+ 
+ puts "-----------"
+ 
+ puts "   |   |   "
+ 
+  puts "-----------"
+ 
+ puts "   |   |   "
   
   
   
